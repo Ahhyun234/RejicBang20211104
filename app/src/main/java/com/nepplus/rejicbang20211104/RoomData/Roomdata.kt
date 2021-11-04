cc5fc46c656a965e1b3cfb12649574462acf5b92
이아현 <suk03029@gmail.com>
2021-11-04 15:31:15 +0900

    끝

--- app/src/main/java/com/nepplus/rejicbang20211104/RoomData/Roomdata.kt
+++ app/src/main/java/com/nepplus/rejicbang20211104/RoomData/Roomdata.kt
@@ -1,4 +1,39 @@
 package com.nepplus.rejicbang20211104.RoomData
 
-class Roomdata(val price:Int, val address:String, val floor:Int, val description:String) {
+import java.lang.NumberFormatException
+import java.text.NumberFormat
+import java.util.*
+
+class Roomdata(val price: Int, val address: String, val floor: Int, val description: String) {
+
+    fun getFormattedPrice(): String {
+
+        if (this.price < 10000) {
+            val priceStr = NumberFormat.getNumberInstance(Locale.KOREA).format(this.price)
+
+            return priceStr
+        } else {
+            val uk = this.price / 10000
+            val rest = this.price % 10000
+            val restCommaStr = NumberFormat.getNumberInstance(Locale.KOREA).format(this.price)
+            val priceStr = "${uk}억 ${restCommaStr}"
+
+            return priceStr
+        }
+
+
+    }
+
+    fun getFormattedFloor(): String {
+
+        if (this.floor > 0) {
+            return "${this.floor}"
+        } else if (this.floor == 0) {
+            return "반지하"
+        } else {
+            return "지하${-this.floor}층"
+        }
+
+
+    }
 }
--- app/src/main/java/com/nepplus/rejicbang20211104/adapters/RoomAdapter.kt
+++ app/src/main/java/com/nepplus/rejicbang20211104/adapters/RoomAdapter.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import android.widget.TextView
 import com.nepplus.rejicbang20211104.R
 import com.nepplus.rejicbang20211104.RoomData.Roomdata
 
@@ -21,6 +22,16 @@ class RoomAdapter(val mcontext:Context, val resId:Int, val mList:ArrayList<Roomd
             tempRow = mInflater.inflate(R.layout.roomitemlinedesign,null)
         }
         val row = tempRow!!
+
+        val roomdata = mList[position]
+
+        val txtPrice = row.findViewById<TextView>(R.id.txtPrice)
+        val txtAddressAndFloor = row.findViewById<TextView>(R.id.txtAddressAndFloor)
+        val txtDescription = row.findViewById<TextView>(R.id.txtDescription)
+
+        txtDescription.text= roomdata.description
+        txtAddressAndFloor.text = "${roomdata.address},${roomdata.getFormattedFloor()}"
+            txtPrice.text = roomdata.getFormattedPrice()
 
         return row
 
