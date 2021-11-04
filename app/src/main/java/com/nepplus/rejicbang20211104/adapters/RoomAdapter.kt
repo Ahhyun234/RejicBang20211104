21bf070280222dfbfdb9e9b26f6200d48d6f67a5
이아현 <suk03029@gmail.com>
2021-11-04 14:48:22 +0900

    RoonAdapter의 인스턴스 선언

--- app/src/main/java/com/nepplus/rejicbang20211104/adapters/RoomAdapter.kt
+++ app/src/main/java/com/nepplus/rejicbang20211104/adapters/RoomAdapter.kt
@@ -1,7 +1,8 @@
 package com.nepplus.rejicbang20211104.adapters
 
+import android.content.Context
 import android.widget.ArrayAdapter
 import com.nepplus.rejicbang20211104.RoomData.Roomdata
 
-class RoomAdapter():ArrayAdapter<Roomdata> {
+class RoomAdapter(val mcontext:Context, val resId:Int, val mList:ArrayList<Roomdata>):ArrayAdapter<Roomdata> {
 }
