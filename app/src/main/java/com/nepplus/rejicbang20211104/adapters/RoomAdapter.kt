8df7230e4ec13876aeb953ff087d371efba59d2c
이아현 <suk03029@gmail.com>
2021-11-04 14:54:37 +0900

    RoonAdapter에서 인플레이터 추가와 겟뷰 오버라이딩

--- app/src/main/java/com/nepplus/rejicbang20211104/adapters/RoomAdapter.kt
+++ app/src/main/java/com/nepplus/rejicbang20211104/adapters/RoomAdapter.kt
@@ -1,8 +1,32 @@
 package com.nepplus.rejicbang20211104.adapters
 
 import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import com.nepplus.rejicbang20211104.R
 import com.nepplus.rejicbang20211104.RoomData.Roomdata
 
-class RoomAdapter(val mcontext:Context, val resId:Int, val mList:ArrayList<Roomdata>):ArrayAdapter<Roomdata> {
+class RoomAdapter(val mcontext:Context, val resId:Int, val mList:ArrayList<Roomdata>)
+    :ArrayAdapter<Roomdata>(mcontext, resId,mList) {
+
+        val mInflater= LayoutInflater.from(mcontext)
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+
+        var tempRow = convertView
+
+        if (tempRow == null ){
+            tempRow = mInflater.inflate(R.layout.roomitemlinedesign,null)
+        }
+        val row = tempRow!!
+
+        return row
+
+    }
+
+
+
+
 }
