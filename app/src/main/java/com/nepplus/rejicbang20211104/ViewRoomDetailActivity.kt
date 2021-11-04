ed391b5032c10b5294d74500eac12609d1d0af28
이아현 <suk03029@gmail.com>
2021-11-04 15:56:05 +0900

    Room 상세화면에 실제 데이터를 표시 -클래스에 만든 함수를 재활용함

--- app/src/main/java/com/nepplus/rejicbang20211104/ViewRoomDetailActivity.kt
+++ app/src/main/java/com/nepplus/rejicbang20211104/ViewRoomDetailActivity.kt
@@ -3,6 +3,7 @@ package com.nepplus.rejicbang20211104
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.nepplus.rejicbang20211104.RoomData.Roomdata
+import kotlinx.android.synthetic.main.activity_view_room_detail.*
 
 class ViewRoomDetailActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,5 +12,9 @@ class ViewRoomDetailActivity : AppCompatActivity() {
 
         val roomData= intent.getSerializableExtra("room") as Roomdata
 
+        txtPrice.text = roomData.getFormattedPrice()
+        txtDescription.text=roomData.description
+        txtAddress.text=roomData.address
+        txtFloor.text=roomData.getFormattedFloor()
     }
 }
