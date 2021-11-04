9b7d64cf306d33c31d837e70befad7168c970fc9
이아현 <suk03029@gmail.com>
2021-11-04 14:46:18 +0900

    adapter 폴더 생성+ Array adapter사용한다고 적음

---
+++ app/src/main/java/com/nepplus/rejicbang20211104/adapters/RoomAdapter.kt
@@ -0,0 +1,7 @@
+package com.nepplus.rejicbang20211104.adapters
+
+import android.widget.ArrayAdapter
+import com.nepplus.rejicbang20211104.RoomData.Roomdata
+
+class RoomAdapter():ArrayAdapter<Roomdata> {
+}
